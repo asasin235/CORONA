01e88a7d7a9e596f4eb9ba86d7190deb4bd988ff
its_D_joker <pandora_box@its-D-jokers-MacBook-Pro.local>
2020-06-11 23:52:42 +0530

    first

---
+++ app/src/main/java/com/example/covidtracker/SpannableDelta.kt
@@ -0,0 +1,16 @@
+package com.example.covidtracker
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+
+class SpannableDelta(langName: String, langColor: String, start: Int) : SpannableString(langName) {
+    init {
+        setSpan(
+            ForegroundColorSpan(Color.parseColor(langColor)),
+            start,
+            langName.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+    }
+}
